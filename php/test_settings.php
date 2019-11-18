5bb6e79086609e6a2ccfe1248b0e0100fdd831b6
Jaiveer Sharma <jaiveersharma12345@gmail.com>
2019-11-17 19:56:06 -0500

    Update test_settings.php

--- php/test_settings.php
+++ php/test_settings.php
@@ -109,22 +109,6 @@ $date = new DateTime();
         <input type="submit" class="button-large" name="submit" value="Submit">
       </form>
 
-      <script>
-        function saveBtn() {
-              $data = array(
-                              "cs_grad_cost" => "0"
-                              "under_grad_cost" => "0"
-                              "grad_cost" => "0"
-                              "cs_grad_cost" => "0"
-                              "doc_cost" => "0"
-                              "cs_doc_cost" => "0"
-                             );
-              $db = new DB();
-              echo $db->update("setting","",$data);
-              alert("The data was submitted");
-                        }
-      </script>
-
     </div>
   </div>
 </body>
