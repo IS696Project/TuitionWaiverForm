d70c637d950eaa11b7679de74e0b2528eb9e4037
Jaiveer Sharma <jaiveersharma12345@gmail.com>
2019-11-18 20:00:14 -0500

    Update settings.php

--- php/settings.php
+++ php/settings.php
@@ -1,16 +1,39 @@
 <?php
+require "db.php";
 $date = new DateTime();
+$flag=0;
+if (isset($_POST['submit'])) {
+    $data = array(
+        "under_grad_cost" => $_POST['under_grad_cost'],
+        "cs_under_grad_cost"  => $_POST['cs_under_grad_cost'],
+        "grad_cost"     => $_POST['grad_cost'],
+        "cs_grad_cost" => $_POST['cs_grad_cost'],
+        "doc_cost"     => $_POST['doc_cost'],
+        "cs_doc_cost" => $_POST['cs_doc_cost'],
+        "due_date" => $_POST['due_date'],
+        "sem_start_date" => $_POST['sem_start_date'],
+    );
+    $db = new DB();
+    $id = $db->update("setting","",$data);
+    if (strval($id)>-1){$flag=1;}
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Search Tuition Waiver Form</title>
-    <link rel="stylesheet" href="css/main.css?ver=<?php echo $date->getTimestamp();?>">
-    <link rel="stylesheet" href="css/component.css?ver=<?php echo $date->getTimestamp();?>">
+    <link rel="stylesheet" href="../css/main.css?ver=<?php echo $date->getTimestamp();?>">
+    <link rel="stylesheet" href="../css/component.css?ver=<?php echo $date->getTimestamp();?>">
     <script src="../js/jquery-3.4.1.min.js"></script>
     <script src="../js/main.js?ver=<?php echo $date->getTimestamp();?>"></script>
     <script src="../js/settings.js?ver=<?php echo $date->getTimestamp();?>"></script>
+    <script>
+      <?php if ($flag==1){ ?>
+      alert("The data is added");
+      <?php } ?>
+    </script>
 </head>
 
 
@@ -22,22 +45,14 @@ $date = new DateTime();
     <div class="title">Settings</div>
     <div class="form">
       <!-- optional and pending review -->
-      <form  action="index.html" method="post">
+      <form onsubmit="return onFormSubmit()" action="test_settings.php" method="post" onSubmit="saveBtn()">
         <table class="form-table" width="50%">
-          <tr>
-            <td>
-              <label for="eid">Employee ID</label>
-            </td>
-            <td>
-              <input placeholder="Employee ID" type="text" id="eid" name="eid">
-            </td>
-          </tr>
           <tr>
             <td>
               <label for="due_date">Due date</label>
             </td>
             <td>
-              <input placeholder="Due Date" type="date" id="due_date" name="due_date">
+              <input type="date" id="due_date" name="due_date">
             </td>
           </tr>
           <tr>
@@ -45,11 +60,11 @@ $date = new DateTime();
               <label for="sem_start_date">Semester Start Date</label>
             </td>
             <td>
-              <input placeholder="Semester Start Date" type="text" id="sem_start_date" name="sem_start_date">
+              <input type="date" id="sem_start_date" name="sem_start_date">
             </td>
           </tr>
 
-          //Undergraduate Costs
+          <!--Undergraduate Costs-->
           <tr>
             <td>
               <label for="under_grad_cost">Undergraduate Cost</label>
@@ -67,7 +82,7 @@ $date = new DateTime();
             </td>
           </tr>
 
-          //Graduate Costs
+          <!--Graduate Costs-->
           <tr>
             <td>
               <label for="grad_cost">Graduate Cost</label>
@@ -85,7 +100,7 @@ $date = new DateTime();
             </td>
           </tr>
 
-          //Doctoral Costs
+          <!--Doctoral Costs-->
           <tr>
             <td>
               <label for="doc_cost">Doctoral Cost</label>
@@ -105,9 +120,12 @@ $date = new DateTime();
 
         </table>
 
+        <br><br>
+        <input type="submit" class="button-large" id="submitBtn" name="submit">
+
       </form>
 
     </div>
-
   </div>
+  <div class="validation-msg">Please fill all required fields</div>
 </body>
